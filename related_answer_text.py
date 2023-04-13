5f9b3725a460cfe90194898fd1ef369169ef9693
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 13:00:26 -0500

    Update related_answer_text.py

--- related_answer_text.py
+++ related_answer_text.py
@@ -1,25 +1,11 @@
-# Split the 'related_answer' column of JISJA23 into individual rows
-# Drop rows with NaN values, and convert each value to integer
-JISJA23 = JISJA23.explode('related_answer').dropna()
-JISJA23['related_answer'] = JISJA23['related_answer'].astype(int)
+# apply the function to the answer_id column in JISJA23
+JISJA23['answer_id'] = JISJA23['answer_id'].apply(convert_to_list)
 
-# Merge JISJA23 with B2B_Questions_Answers on 'answer_id'
-merged = pd.merge(JISJA23, B2B_Questions_Answers, how='left', left_on='related_answer', right_on='answer_id')
+# merge the JISJA23 and B2B_Questions_Answers datasets on the 'answer_id' column
+merged_df = pd.merge(JISJA23, B2B_Questions_Answers, left_on='answer_id', right_on='answer_id', how='left')
 
-# Group by the index of the original JISJA23 dataframe, and aggregate the related_answer_text column as a list
-grouped = merged.groupby(merged.index)['answer_text'].agg(list)
+# group the merged dataframe by 'answer_id' and 'question_id', and aggregate the 'answer_text' column
+grouped_df = merged_df.groupby(['answer_id', 'question_id'])['answer_text'].apply(list).reset_index(name='related_answer_text')
 
-# Join the grouped data with the original JISJA23 dataframe
-JISJA23 = JISJA23.join(grouped, how='left')
-
-# Rename the related_answer_text column to related_answer_text_list
-JISJA23 = JISJA23.rename(columns={'answer_text': 'related_answer_text_list'})
-
-# Create a new column 'related_answer_text' by converting the list to a string
-JISJA23['related_answer_text'] = JISJA23['related_answer_text_list'].apply(lambda x: ','.join(map(str, x)) if isinstance(x, list) else '')
-
-# Drop the related_answer_text_list column
-JISJA23 = JISJA23.drop(columns=['related_answer_text_list'])
-
-# Save the result to a new csv file
-JISJA23.to_csv('path/to/output.csv', index=False)
+# merge the grouped dataframe back into the original JISJA23 dataframe
+final_df = pd.merge(JISJA23, grouped_df, on=['answer_id', 'question_id'], how='left')
