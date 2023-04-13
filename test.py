2bcfcf4822b6eb33eb36908f04c70b9518ea695a
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 09:55:21 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,13 @@
-import pandas as pd
-
-# Load the DataFrame
-df = pd.read_csv('your_csv_file.csv')
-
-# Define a list to store the answer IDs
-answer_ids_list = []
-
-for index, row in df.iterrows():
-    answer_ids_str = json.loads(row['profileresponses'])[0]['answerIds']
-    if answer_ids_str:
-        answer_ids = [int(id) for id in answer_ids_str if id.isdigit()]
+def extract_answerIds(row):
+    answer_ids_str = row['profileresponses']
+    answer_ids = re.findall(r'"answerIds":\s*\[([\d,\s]*)\]', answer_ids_str)
+    if answer_ids:
+        answer_ids = answer_ids[0].split(',')
+        answer_ids = [int(id) for id in answer_ids if id.strip().isdigit()]
     else:
         answer_ids = []
-    df.at[index, 'answerIds'] = answer_ids
-    
-    
-# Add a new column to the DataFrame to store the answer IDs
-df['answerIds'] = answer_ids_list
+    return answer_ids
+
+df['answerIds'] = df.apply(extract_answerIds, axis=1)
+
+print(df.head())
