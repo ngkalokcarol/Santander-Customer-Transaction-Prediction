38c4d5c86be21cf6c797280f0d7aa54927dad96f
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 12:01:25 -0500

    Update get_related_answer.py

--- get_related_answer.py
+++ get_related_answer.py
@@ -1,12 +1,12 @@
-def get_related_answer(answer_id, df):
-    if pd.isna(answer_id):
-        return None
-    else:
+def get_related_answer(row):
+    if isinstance(row['answer_id'], list) and row['related_answer'] is None:
         related_answer = []
-        for ans in answer_id:
-            if ans in df['answer_id'].tolist():
-                related_answer.append(ans)
+        for ans_id in row['answer_id']:
+            if ans_id in df3['answer_id'].values:
+                related_answer.append(ans_id)
         return related_answer if related_answer else None
+    else:
+        return row['related_answer']
 
 # create the related_answer column in JISJA23_2
 JISJA23['related_answer'] = JISJA23['answer_id'].apply(get_related_answer, args=(df3,))
