b406fa6cbf5b0d1477b92664925e092d20513fde
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 09:51:01 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,30 @@
-# Merge transform_df with question_answer_list on 'answer_id'
-merged_df = transform_df.merge(question_answer_list, on='answer_id')
+import pandas as pd
 
-# Pivot the merged_df to create the transformed_question_list
-transformed_question_list_df = merged_df.pivot(index='question_id', columns='badgenumber', values='answer_text').reset_index().rename(columns={'question_id': ''})
+# Load the DataFrame
+df = pd.read_csv('your_csv_file.csv')
 
-# Merge the transformed_question_list with the master table on 'badgenumber'
-master_table_df = transformed_question_list_df.merge(transform_df[['badgenumber', 'answer_id']], on='badgenumber')
+# Define a list to store the answer IDs
+answer_ids_list = []
 
-# Iterate through each row of the master_table_df and update the corresponding value in transformed_question_list_df
-for index, row in master_table_df.iterrows():
-    badgenumber = row['badgenumber']
-    for col in transformed_question_list_df.columns[1:]:
-        answer_id = row['answer_id']
-        question_id = col
-        if pd.notna(answer_id) and answer_id in question_answer_list[question_answer_list['question_id'] == question_id]['answer_id'].tolist():
-            answer_text = merged_df.loc[(merged_df['answer_id'] == answer_id) & (merged_df['question_id'] == question_id), 'answer_text'].iloc[0]
-            transformed_question_list_df.loc[index, col] = answer_text
+# Iterate over the rows of the DataFrame
+for index, row in df.iterrows():
+    # Get the value of 'answerIds' for the given row
+    answer_ids_str = row['profileresponses'].get('answerIds')
+    
+    # Check if answer_ids_str is empty or None
+    if answer_ids_str:
+        # Split the answer IDs string into a list of individual IDs
+        answer_ids = answer_ids_str.split(',')
+        
+        # Convert each answer ID to an integer and add it to the answer_ids_list
+        for answer_id in answer_ids:
+            try:
+                answer_ids_list.append(int(answer_id))
+            except ValueError:
+                pass
+    else:
+        # If answerIds is empty, append None to the answer_ids_list
+        answer_ids_list.append(None)
 
-# Set the index of the transformed_question_list_df to 'badgenumber'
-transformed_question_list_df.set_index('badgenumber', inplace=True)
+# Add a new column to the DataFrame to store the answer IDs
+df['answerIds'] = answer_ids_list
