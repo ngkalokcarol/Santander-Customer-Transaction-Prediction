c5bfe5b1117bf48da3c75506e690b0855d1e812c
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 12:38:32 -0500

    Update get_related_answer.py

--- get_related_answer.py
+++ get_related_answer.py
@@ -13,16 +13,17 @@ JISJA23['answer_id'] = JISJA23['answer_id'].apply(convert_to_list)
 
 
 
-# define a function to get the related answers
-def get_related_answer(row, df):
-    if isinstance(row['answer_id'], list) and pd.isnull(row['related_answer']):
+# define a function to check if the answer id is in df3
+def get_related_answer(row):
+    if isinstance(row['answer_id'], list) and row['related_answer'] is None:
         related_answer = []
         for ans_id in row['answer_id']:
-            if ans_id in df['answer_id'].values:
+            if str(ans_id) in df3['answer_id'].values:
                 related_answer.append(ans_id)
-        return related_answer if related_answer else np.nan
+        return related_answer if related_answer else None
     else:
         return row['related_answer']
-
-# apply the function to JISJA23
-JISJA23['related_answer'] = JISJA23.apply(get_related_answer, args=(df3,), axis=1)
+    
+# create the related_answer column in JISJA23
+JISJA23['related_answer'] = None
+JISJA23['related_answer'] = JISJA23.apply(get_related_answer, axis=1)
