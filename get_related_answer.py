4381b789c97cdde356a06609130cc80a2a2ae3ab
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 11:50:11 -0500

    Update get_related_answer.py

--- get_related_answer.py
+++ get_related_answer.py
@@ -1,1 +1,12 @@
-JISJA23_2['related_answer'] = JISJA23_2['answer_id'].apply(lambda x: [aid for aid in x if aid in df3['answer_id'].tolist()])
+def get_related_answer(row):
+    if not pd.isna(row['answer_id']):
+        answer_ids = [int(id) for id in row['answer_id']]
+        related_answers = []
+        for answer_id in answer_ids:
+            if answer_id in df3['answer_id'].values:
+                related_answers.append(answer_id)
+        return related_answers
+    else:
+        return None
+
+JISJA23_2['related_answer'] = JISJA23_2.apply(get_related_answer, axis=1)
