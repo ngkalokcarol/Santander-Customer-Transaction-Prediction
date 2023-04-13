10bb93c866411735852584cc9fbf9942af2ff1a7
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 12:06:50 -0500

    Update get_related_answer.py

--- get_related_answer.py
+++ get_related_answer.py
@@ -1,10 +1,14 @@
-def get_related_answer(row):
-    if isinstance(row['answer_id'], list) and row['related_answer'] is None:
+def get_related_answer(row, df):
+    """
+    Function to match the answer IDs between two data frames and create a new column 'related_answer' in the first data
+    frame with the matching IDs.
+    """
+    if isinstance(row['answer_id'], list) and pd.isna(row['related_answer']):
         related_answer = []
         for ans_id in row['answer_id']:
-            if ans_id in df3['answer_id'].values:
+            if ans_id in df['answer_id'].values:
                 related_answer.append(ans_id)
-        return related_answer if related_answer else None
+        return related_answer if related_answer else np.nan
     else:
         return row['related_answer']
 
