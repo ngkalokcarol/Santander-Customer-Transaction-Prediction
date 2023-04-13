52db773f37c1b8354d97b112e4c44e8461efed01
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 09:53:32 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,25 +6,14 @@ df = pd.read_csv('your_csv_file.csv')
 # Define a list to store the answer IDs
 answer_ids_list = []
 
-# Iterate over the rows of the DataFrame
 for index, row in df.iterrows():
-    # Get the value of 'answerIds' for the given row
-    answer_ids_str = row['profileresponses'].get('answerIds')
-    
-    # Check if answer_ids_str is empty or None
+    answer_ids_str = json.loads(row['profileresponses'])[0]['answerIds']
     if answer_ids_str:
-        # Split the answer IDs string into a list of individual IDs
-        answer_ids = answer_ids_str.split(',')
-        
-        # Convert each answer ID to an integer and add it to the answer_ids_list
-        for answer_id in answer_ids:
-            try:
-                answer_ids_list.append(int(answer_id))
-            except ValueError:
-                pass
+        answer_ids = [int(id) for id in answer_ids_str if id.isdigit()]
     else:
-        # If answerIds is empty, append None to the answer_ids_list
-        answer_ids_list.append(None)
-
+        answer_ids = []
+    df.at[index, 'answerIds'] = answer_ids
+    
+    
 # Add a new column to the DataFrame to store the answer IDs
 df['answerIds'] = answer_ids_list
