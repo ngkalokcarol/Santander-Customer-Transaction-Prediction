41dbbd32a4156295a49bc855fdfc5049a7f6ebea
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 13:03:46 -0500

    Update related_answer_text.py

--- related_answer_text.py
+++ related_answer_text.py
@@ -1,26 +1,17 @@
-# Merge the JISJA23 and B2B_Questions_Answers datasets on the 'answer_id' column
+# explode the lists in JISJA23
+JISJA23 = JISJA23.explode('related_answer')
+
+# join the dataframes using 'answer_id' column in B2B_Questions_Answers and 'related_answer' in JISJA23
 merged_df = JISJA23.merge(B2B_Questions_Answers, left_on='related_answer', right_on='answer_id', how='left')
 
-# Create an empty list to store the related answer texts
-related_answer_text = []
+# create new column 'related_answer_text' containing answer_text values
+merged_df['related_answer_text'] = merged_df['answer_text']
 
-# Iterate over the rows in the merged dataframe
-for i, row in merged_df.iterrows():
-    # Get the related answers for the current row
-    related_answers = row['related_answer']
-    
-    # If there are no related answers, append an empty list to the related_answer_text list
-    if pd.isna(related_answers):
-        related_answer_text.append([])
-    else:
-        # Split the related answers into a list of integers
-        related_answers = [int(x) for x in related_answers.split(',')]
-        
-        # Get the corresponding answer texts from B2B_Questions_Answers
-        answer_texts = B2B_Questions_Answers.loc[B2B_Questions_Answers['answer_id'].isin(related_answers), 'answer_text'].tolist()
-        
-        # Append the answer texts to the related_answer_text list
-        related_answer_text.append(answer_texts)
-        
-# Add the related_answer_text column to the merged dataframe
-merged_df['related_answer_text'] = related_answer_text
+# group the rows by the original index and aggregate the 'related_answer_text' values into lists
+merged_df = merged_df.groupby(merged_df.index)['related_answer_text'].apply(list)
+
+# merge the aggregated 'related_answer_text' column with the original dataframe
+JISJA23 = pd.concat([JISJA23, merged_df], axis=1)
+
+# print the resulting dataframe
+print(JISJA23)
