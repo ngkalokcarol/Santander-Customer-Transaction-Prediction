d22838943c577a4618dca1d3a1ea828e4c98a112
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 11:55:49 -0500

    Update get_related_answer.py

--- get_related_answer.py
+++ get_related_answer.py
@@ -1,12 +1,5 @@
-def get_related_answer(row):
-    if not pd.isna(row['answer_id']):
-        answer_ids = [int(id) for id in row['answer_id']]
-        related_answers = []
-        for answer_id in answer_ids:
-            if answer_id in df3['answer_id'].values:
-                related_answers.append(answer_id)
-        return related_answers
-    else:
-        return None
+# create a set of unique answer_ids in df3
+df3_answer_ids = set(df3['answer_id'])
 
-JISJA23_2['related_answer'] = JISJA23_2.apply(get_related_answer, axis=1)
+# create a new column 'related_answer' in JISJA23_2
+JISJA23_2['related_answer'] = JISJA23_2['answer_id'].apply(lambda x: [val for val in x if val in df3_answer_ids])
