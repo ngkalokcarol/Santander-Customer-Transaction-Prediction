c24cfc0d29a7b52555b5c109ced23f6a33aca23c
Carol Ng <50436546+ngkalokcarol@users.noreply.github.com>
2023-04-13 11:47:23 -0500

    Update get_related_answer.py

--- get_related_answer.py
+++ get_related_answer.py
@@ -1,8 +1,1 @@
-def get_related_answer(answer_ids):
-    related_answer = []
-    for answer_id in answer_ids:
-        if answer_id in df3['question_id'].values:
-            related_answer.append(answer_id)
-    return related_answer
-
-JISJA23_3['related_answer'] = JISJA23_3['answerIds'].apply(lambda answer_ids: [df3.loc[df3['question_id'] == id, 'answer'].iloc[0] for id in answer_ids if (str(id).isdigit() and df3['question_id'].eq(int(id)).any())]
+JISJA23_2['related_answer'] = JISJA23_2['answer_id'].apply(lambda x: [aid for aid in x if aid in df3['answer_id'].tolist()])
